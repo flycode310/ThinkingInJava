c509987438c7bf9500292c10d0fe4a2a3ef2ace8
flycode310 <cyy_ahut@yeah.net>
2015-06-15 11:06:29 +0800

    AtomicEvenGenerator.java AtomicIntegerTest.java

---
+++ src/concurrency/AtomicEvenGenerator.java
@@ -0,0 +1,13 @@
+package concurrency;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicEvenGenerator extends IntGenerator {
+	private AtomicInteger currentEvenValue = new AtomicInteger();
+	public int next() {
+		return currentEvenValue.addAndGet(2);
+	}
+	public static void main(String[] args) {
+		EvenChecker.test(new AtomicEvenGenerator());
+	}
+}
