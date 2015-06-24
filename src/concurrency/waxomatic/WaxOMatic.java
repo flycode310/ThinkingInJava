911b09277836b4d252cd6aece34aacf5677f00c8
chenyayun <cyy_ahut@yeah.net>
2015-06-24 19:13:22 +0800

    NotifyVsNotifyAll.java and WaxOMatic.java

---
+++ src/concurrency/waxomatic/WaxOMatic.java
@@ -0,0 +1,73 @@
+package concurrency.waxomatic;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Car {
+	private boolean waxOn = false;
+	public synchronized void waxed() {
+		waxOn = true;
+		notifyAll();
+	}
+	
+	public synchronized void buffed() {
+		waxOn = false;
+		notifyAll();
+	}
+	
+	public synchronized void waitForWaxing() throws InterruptedException {
+		while (waxOn == false)
+			wait();
+	}
+	public synchronized void waitForBuffing() throws InterruptedException {
+		while (waxOn == true)
+			wait();
+	}
+}
+
+class WaxOn implements Runnable {
+	private Car car;
+	public WaxOn(Car c) { car = c; }
+	public void run() {
+		try {
+			while (!Thread.interrupted()) {
+				System.out.println("Wax On! ");
+				TimeUnit.MILLISECONDS.sleep(200);
+				car.waxed();
+				car.waitForBuffing();
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Exing via interrupt ");
+		}
+		System.out.println("Ending Wax On tast");
+	}
+}
+
+class WaxOff implements Runnable {
+	private Car car;
+	public WaxOff(Car c) { car = c; }
+	public void run() {
+		try {
+			while (!Thread.interrupted()) {
+				car.waitForWaxing();
+				System.out.println("Wax Off");
+				TimeUnit.MILLISECONDS.sleep(200);
+				car.buffed();
+			} 
+			}catch (InterruptedException e) {
+				System.out.println("Exiting via interrupt");
+		}
+		System.out.println("Exiting Wax Off task");
+	}
+}
+public class WaxOMatic {
+	public static void main(String[] args) throws Exception {
+		Car car = new Car();
+		ExecutorService exec = Executors.newCachedThreadPool();
+		exec.execute(new WaxOff(car));
+		exec.execute(new WaxOn(car));
+		TimeUnit.SECONDS.sleep(5);
+		exec.shutdownNow();
+	}
+}
